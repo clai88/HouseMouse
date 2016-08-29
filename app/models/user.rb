bee06a36e4ad57b49b8c7a5589ed09ff8eaa6adb
Chris <clai88@gmail.com>
2016-08-29 19:54:48 -0400

    able to send confirmation email now

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable #:confirmable
+  :recoverable, :rememberable, :trackable, :validatable
+  devise :registerable, :confirmable
 
   has_many :user_houses
   has_many :houses, through: :user_houses
