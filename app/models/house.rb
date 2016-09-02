a6a50fe00e81fe9459a5a49fd68a92545a7e1744
Chris <clai88@gmail.com>
2016-09-02 15:54:30 -0400

    expanded on active_record validation (address and zip code length)

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,6 @@
 class House < ApplicationRecord
-  validates :street_address, presence: true, uniqueness: { scope: :zip }
-  validates :zip, presence: true, numericality: true, length: { maximum: 8, message: "That zip code doesn't look right.  Try a different one" }
+  validates :street_address, presence: true, uniqueness: { scope: :zip }, length: { maximum: 95, message: "That address code doesn't look right.  Try a different one" }
+  validates :zip, presence: true, numericality: true, length: { maximum: 9, message: "That zip code doesn't look right.  Try a different one" }
 
   def street_address=(string)
     super(string.downcase.strip)
