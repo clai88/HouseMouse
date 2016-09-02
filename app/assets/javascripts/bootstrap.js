91f474daeb32714e4150f69c890a76ef8df052af
Chris <clai88@gmail.com>
2016-09-02 14:35:00 -0400

    moved imports to app.html

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -4,10 +4,8 @@
 * Licensed under the MIT license
 */
 
-// <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'>
-// </script>
 
-// <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+
 
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
