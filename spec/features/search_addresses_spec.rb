16b93a91a7cced142e1685a8e42bd42a412bb41d
Chris <clai88@gmail.com>
2016-09-01 21:00:53 -0400

    added integration tests

--- spec/features/search_addresses_spec.rb
+++ spec/features/search_addresses_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "SearchAddresses", :type => :feature, :js => true do
-  it "signs me in" do
+  it "search basic address" do
     visit root_path
 
     fill_in('Street address', with: '10823 Maplecrest lane')
@@ -9,7 +9,28 @@ feature "SearchAddresses", :type => :feature, :js => true do
     sleep 2
     click_button('Submit')
 
-    assert page.has_content?("WalkScore")
-    assert page.has_content?("Last Sold Price =")
+
+    within ".house-info" do
+      assert page.has_content?("Last Sold Price =")
+    end
+
+    within ".walkscore" do
+      assert page.has_content?("WalkScore")
+    end
+    sleep 1
+  end
+end
+
+feature "SignUpUser", :type => :feature, :js => true do
+  it "sign up" do
+    visit new_user_registration_path
+
+    fill_in('Email', with: 'clai88@gmail.com')
+    fill_in('Password', with: 'clai88')
+    fill_in('Password confirmation', with: 'clai88')
+
+    assert_difference "Company.count" do
+      click_button('Sign Up')
+    end
   end
 end
