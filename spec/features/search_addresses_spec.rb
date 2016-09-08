13cc992569377a11afecefd747bd2122fb6b01d4
Chris <clai88@gmail.com>
2016-09-08 13:40:59 -0400

    fixed email confirmation forms and buttons to better match overall theme

--- spec/features/search_addresses_spec.rb
+++ spec/features/search_addresses_spec.rb
@@ -10,6 +10,7 @@ feature "SearchAddresses", :type => :feature, :js => true do
     click_button('Submit')
 
 
+    sleep 3
     within ".house-info" do
       assert page.has_content?("Last Sold Price =")
     end
@@ -17,20 +18,34 @@ feature "SearchAddresses", :type => :feature, :js => true do
     within ".walkscore" do
       assert page.has_content?("WalkScore")
     end
-    sleep 1
+    sleep 3
   end
 end
 
 feature "SignUpUser", :type => :feature, :js => true do
-  it "sign up" do
+  it "sign up repeated email" do
     visit new_user_registration_path
 
-    fill_in('Email', with: 'clai88@gmail.com')
+    fill_in('Email', with: '1@1@gmail.com')
     fill_in('Password', with: 'clai88')
     fill_in('Password confirmation', with: 'clai88')
 
     click_button('Sign Up')
 
     assert page.has_content?("Email has already been taken")
+    sleep 4
+  end
+end
+
+feature "SignInUser", :type => :feature, :js => true do
+  it "sign up unconfirmed email" do
+    visit new_user_session_path
+
+    fill_in('Email', with: 'clai88@gmail.com')
+    fill_in('Password', with: 'clai88')
+
+    click_button('Log In')
+    assert page.has_content?("You have to confirm your email address before continuing.")
+
   end
 end
